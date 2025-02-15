730283567ff9bee9c1f242260b51294f5e297aef
kimthon <xoghks9793@gmail.com>
2025-02-15 15:28:53 +0900

    feat : Add LDD module
    - add LDD module config(assignment-7)
    - use overlay for scripts

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,15 @@
+##############################################################
+#
+# LDD
+#
+##############################################################
+
+LDD_VERSION = 'aa488f2aa2b1c995bbc37f0e9e8fddd6fce55db8'
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-kimthon.git'
+
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+
+LDD_MODULE_SUBDIRS = misc-modules scull
+
+$(eval $(kernel-module))
